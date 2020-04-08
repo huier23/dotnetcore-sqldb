3c388472555a242520d7f48e455ce549ab5585fa
Where <huier23@gmail.com>
2020-04-08 21:47:03 +0800

    edit database

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,9 @@ namespace DotNetCoreSqlDb
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
+                    options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
+            // Automatically perform database migration
+            services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
